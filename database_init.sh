b132da53eccf1a71599a3459713ddbb9621eb954
SergeiIonin <sai1042@yandex.ru>
2024-10-19 22:59:27 -0300

    fix database_init.sh

--- database_init.sh
+++ database_init.sh
@@ -1,6 +1,9 @@
-#/bin/bash
+#!/bin/bash
 
 CONTAINER_NAME="mongo"
+DB_NAME="docker_sandbox"
+COLLECTION="composes"
+
 CONTAINER_ID=$(docker ps -q --filter "name=$CONTAINER_NAME")
 
 if [ -z "$CONTAINER_ID" ]; then
@@ -8,11 +11,15 @@ if [ -z "$CONTAINER_ID" ]; then
   exit 1
 fi
 
-# Execute the mongo shell command within the container to create a database
-docker exec -it $CONTAINER_ID mongosh --eval "use docker_sandbox"
-
-if [ $? -eq 0 ]; then
-  echo "Database docker_sandbox created successfully."
+COLLECTION_EXISTS=$(docker exec $CONTAINER_ID mongosh --quiet --eval "db = db.getSiblingDB('$DB_NAME'); db.getCollectionNames().includes('$COLLECTION')")
+if [ "$COLLECTION_EXISTS" = "true" ]; then
+  echo "Collection '$COLLECTION' already exists in database '$DB_NAME'. Skipping creation."
 else
-  echo "Failed to create database docker_sandbox."
+  docker exec -it $CONTAINER_ID mongosh --eval "db = db.getSiblingDB('$DB_NAME'); db.createCollection('$COLLECTION');"
+  
+  if [ $? -eq 0 ]; then
+    echo "Collection '$COLLECTION' created successfully in database '$DB_NAME'."
+  else
+    echo "Failed to create collection '$COLLECTION' in database '$DB_NAME'."
+  fi
 fi
